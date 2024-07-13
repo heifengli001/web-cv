820d63b22499f1a1e02f39ec42057760e09b9e87
heifengli001 <13675665+heifengli001@users.noreply.github.com>
2024-07-13 14:57:48 +0800

    ✨ feat(TypeScript): 新增通用方法
    防抖，节流，缓存，is

--- typescript/README.md
+++ typescript/README.md
@@ -1,1 +1,6 @@
 # TypeScript-CV
+
++ debounce 防抖 [./debounce.ts](./debounce.ts)
++ throttle 节流  [./throttle.ts](./throttle.ts)
++ storage 浏览器存储操作 [./storage.ts](./storage.ts)
++ is 判断类型 [./is.ts](./is.ts)
---
+++ typescript/is.ts
@@ -0,0 +1,35 @@
+
+const opt = Object.prototype.toString;
+
+export function  isObject(value) {
+  var type = typeof value;
+  return value != null && (type == 'object' || type == 'function');
+}
+
+export function isFile(obj: any): obj is File {
+  return opt.call(obj) === '[object File]';
+}
+
+export function isBlob(obj: any): obj is Blob {
+  return opt.call(obj) === '[object Blob]';
+}
+
+export function isEmptyObject(obj: any): boolean {
+  return isObject(obj) && Object.keys(obj).length === 0;
+}
+
+export function isExist(obj: any): boolean {
+  return obj || obj === 0;
+}
+
+export function isWindow(el: any): el is Window {
+  return el === window;
+}
+
+export function isUrl(url: string): boolean {
+  const regexUrl = new RegExp(
+    '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$',
+    'i'
+  );
+  return regexUrl.test(url);
+}
